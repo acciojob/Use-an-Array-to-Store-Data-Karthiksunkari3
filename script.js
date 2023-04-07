c709b6ecf173ddfa694772805386ebef9e87efa7
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-04-08 00:29:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray.length);
+// logs 7
 
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ]
+];
+
+let yourArray=['karthik',22,false,true,23];
+console.log(simpleArray.length);
